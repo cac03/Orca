916ca50a911f14e1ebace8723d976570faba13c4
cac03 <happy96538@gmail.com>
2016-11-24 23:43:59 +0300

    Define base interface for presenter and view

---
+++ app/src/main/java/com/caco3/orca/mvp/BasePresenter.java
@@ -0,0 +1,19 @@
+package com.caco3.orca.mvp;
+
+/**
+ * Base interface for presenters
+ *
+ * @param <V> view type
+ */
+public interface BasePresenter<V> {
+    /**
+     * Called when view attached to this presenter
+     * @param view to attach
+     */
+    void onViewAttached(V view);
+
+    /**
+     * Called when detached from this presenter
+     */
+    void onViewDetached();
+}
---
+++ app/src/main/java/com/caco3/orca/mvp/BaseView.java
@@ -0,0 +1,15 @@
+package com.caco3.orca.mvp;
+
+/**
+ * Base interface for views.
+ * Mvp architectural pattern
+ *
+ * @param <P> type of presenter which will be associated with this view
+ */
+public interface BaseView<P> {
+    /**
+     * Associates this view with provided presenter
+     * @param presenter
+     */
+    void setPresenter(P presenter);
+}
