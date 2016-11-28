d90719c90cf57f19b7f1f31f40dc08b3eef26e92
cac03 <happy96538@gmail.com>
2016-11-28 23:14:45 +0300

    Add empty LearningActivity Add all activities to manifest

---
+++ app/src/main/java/com/caco3/orca/learning/LearningActivity.java
@@ -0,0 +1,16 @@
+package com.caco3.orca.learning;
+
+import android.os.Bundle;
+
+import com.caco3.orca.R;
+import com.caco3.orca.ui.BaseActivity;
+
+
+public class LearningActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
