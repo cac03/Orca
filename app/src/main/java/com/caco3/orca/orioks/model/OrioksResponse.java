6ea4b94ada3c17ac7e821054a6ddea347b389bd1
cac03 <happy96538@gmail.com>
2016-11-29 19:44:26 +0300

    Add methods to OrioksResponse interface

--- app/src/main/java/com/caco3/orca/orioks/model/OrioksResponse.java
+++ app/src/main/java/com/caco3/orca/orioks/model/OrioksResponse.java
@@ -1,9 +1,36 @@
 package com.caco3.orca.orioks.model;
 
 
+import java.util.List;
+
 /**
  * Interface for response returned by {@link com.caco3.orca.orioks.Orioks}
  */
 public interface OrioksResponse {
-    // TODO: 11/27/16 define interface
+
+    /**
+     * Returns a list of disciplines for user who credentials was used to get this response
+     * @return list
+     */
+    List<Discipline> getDisciplines();
+
+    /**
+     * Returns a {@link Student} associated in the university with credentials used to get this respose
+     * @return {@link Student}
+     */
+    Student getStudent();
+
+    /**
+     * Returns number of current semester for {@link Student}
+     * associated in the university with credentials used to get this respose
+     *
+     * @return int
+     */
+    int getCurrentSemester();
+
+    /**
+     * Returns current week in the university
+     * @return int
+     */
+    int getCurrentWeek();
 }
