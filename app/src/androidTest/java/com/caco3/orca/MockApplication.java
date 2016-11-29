3d3f44f9baa71ecef2905e99144e341fbae86f73
cac03 <happy96538@gmail.com>
2016-11-29 23:50:25 +0300

    Fix building app component in MockApplication

--- app/src/androidTest/java/com/caco3/orca/MockApplication.java
+++ app/src/androidTest/java/com/caco3/orca/MockApplication.java
@@ -1,6 +1,10 @@
 package com.caco3.orca;
 
 
+import android.content.Context;
+
+import com.caco3.orca.credentials.CredentialsManager;
+import com.caco3.orca.credentials.CredentialsModule;
 import com.caco3.orca.orioks.Orioks;
 import com.caco3.orca.orioks.OrioksModule;
 
@@ -11,10 +15,17 @@ public class MockApplication extends OrcaApp {
     @Override
     protected DaggerApplicationComponent.Builder prepareApplicationComponent() {
         return DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
                 .orioksModule(new OrioksModule(){
                     @Override
                     public Orioks provideOrioks() {
                         return Mockito.mock(Orioks.class);
+                    }
+                })
+                .credentialsModule(new CredentialsModule(){
+                    @Override
+                    public CredentialsManager provideCredentialsManager(Context context) {
+                        return Mockito.mock(CredentialsManager.class);
                     }
                 });
     }
