10c912c449fd3dc76b90aac1167d23720bcbec4d
cac03 <happy96538@gmail.com>
2016-12-04 22:02:23 +0300

    Redefine schedule view contract

--- app/src/main/java/com/caco3/orca/schedule/ScheduleView.java
+++ app/src/main/java/com/caco3/orca/schedule/ScheduleView.java
@@ -9,16 +9,47 @@ import java.util.List;
 /*package*/ interface ScheduleView extends BaseView<SchedulePresenter> {
 
     /**
-     * Shows/hides {@link android.support.v4.widget.SwipeRefreshLayout}
+     * When this view run for the first time, we try to predict the group the app user studies in,
+     * if we couldn't do it, we must notify him about it and suggest to select it manually
+     */
+    void showCouldntPredictGroupErrorView();
+
+    /**
+     * Called when an error occurred while we were trying to load group names
+     * schedule for is available
+     */
+    void showCouldntLoadGroupsErrorView();
+
+    /**
+     * Called when we were unable to load schedule for group because of i/o error or.. parse error
+     * @param groupName we couldn't load schedule for
+     */
+    void showCouldntGetScheduleErrorView(String groupName);
+
+    /**
+     * Shows schedule view for group name
+     * @param groupName schedule for is shown
+     */
+    void showScheduleView(String groupName);
+
+    /**
+     * Controls {@link android.support.v4.widget.SwipeRefreshLayout} refreshing state.
+     * Must be used to indicate that we're really updating schedule for current set group
      * @param refreshing true to show, false to hide
      */
     void setRefreshing(boolean refreshing);
 
     /**
-     * Sets items to show in view
-     * @param items to show
+     * Shows schedule list from provided {@link DaySchedule} list
+     * @param schedule to show
      */
-    void setItems(List<DaySchedule> items);
+    void showSchedule(List<DaySchedule> schedule);
+
+    /**
+     * Opens dialog where user is able to select a group schedule for he wants to see
+     * @param groupNames list of group names schedule for is available
+     */
+    void openSelectGroupDialog(List<String> groupNames);
 
     /**
      * Notifies user about network unavailability
@@ -36,8 +67,14 @@ import java.util.List;
     void sayParseErrorOccurred();
 
     /**
-     * Suggests user to select group since there is no group selected
-     * @param groupNames to select from
+     * Shows {@link android.widget.ProgressBar}.
+     * It must be used to show that we're loading group names schedule is available for,
+     * or we're loading schedule for another group
      */
-    void suggestToSelectGroupToShowScheduleFor(List<String> groupNames);
+    void showProgress();
+
+    /**
+     * Counterpart of {@link #showProgress()}
+     */
+    void hideProgress();
 }
