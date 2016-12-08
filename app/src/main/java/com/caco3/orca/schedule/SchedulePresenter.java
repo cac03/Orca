b75318e8170f1c1ebcde9cbf58c9b3b7479a9f18
cac03 <happy96538@gmail.com>
2016-12-08 20:09:09 +0300

    Change schedule presenter contract

--- app/src/main/java/com/caco3/orca/schedule/SchedulePresenter.java
+++ app/src/main/java/com/caco3/orca/schedule/SchedulePresenter.java
@@ -2,6 +2,9 @@ package com.caco3.orca.schedule;
 
 import com.caco3.orca.mvp.BasePresenter;
 
+/**
+ * Presenter interface for schedule view
+ */
 /*package*/ interface SchedulePresenter extends BasePresenter<ScheduleView> {
 
     /**
@@ -13,5 +16,27 @@ import com.caco3.orca.mvp.BasePresenter;
      * Called when user selects a group name to show schedule for from suggested
      * @param groupName group name selected by user
      */
-    void onGroupToShowScheduleForSelected(String groupName);
+    void onGroupSelected(String groupName);
+
+    /**
+     * Called when user wants to change a group schedule for he wants to see
+     */
+    void onChangeGroupRequest();
+
+    /**
+     * When user minimizes the app view is still attached, user can resume app in long period later,
+     * and if the day changed for example, some schedule items may become invalid, so we have to
+     * remove invalid items if necessary when view becomes visible
+     */
+    void onViewVisible();
+
+    /**
+     * Called when the user clicks retry button in 'could not load groups' view
+     */
+    void retryLoadGroups();
+
+    /**
+     * Called when the user clicks retry button in 'could not load schedule' view
+     */
+    void retryToLoadSchedule();
 }
