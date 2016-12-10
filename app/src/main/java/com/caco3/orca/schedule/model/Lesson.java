62f3ea4214e1e672b6d4b6d2a44bc39d9cc5e929
cac03 <happy96538@gmail.com>
2016-12-10 15:38:10 +0300

    Add builder

--- app/src/main/java/com/caco3/orca/schedule/model/Lesson.java
+++ app/src/main/java/com/caco3/orca/schedule/model/Lesson.java
@@ -1,5 +1,7 @@
 package com.caco3.orca.schedule.model;
 
+import com.caco3.orca.util.Preconditions;
+
 /**
  * A lesson model
  * Used in views to show schedule. It's a part of {@link DaySchedule}
@@ -44,6 +46,24 @@ public class Lesson {
         this.isLaboratoryWork = isLaboratoryWork;
         this.isPhysicalEducation = isPhysicalEducation;
         this.isMilitaryLesson = isMilitaryLesson;
+    }
+
+    private Lesson(Builder builder) {
+        this.disciplineName = builder.disciplineName;
+        this.beginMillis = builder.beginMillis;
+        this.endMillis = builder.endMillis;
+        this.classroom = builder.classroom;
+        this.teacherName = builder.teacherName;
+        this.orderInDay = builder.orderInDay;
+        this.isLecture = builder.isLecture;
+        this.isSeminar = builder.isSeminar;
+        this.isLaboratoryWork = builder.isLaboratoryWork;
+        this.isPhysicalEducation = builder.isPhysicalEducation;
+        this.isMilitaryLesson = builder.isMilitaryLesson;
+    }
+
+    public static Builder builder(){
+        return new Builder();
     }
 
     public String getDisciplineName() {
@@ -113,5 +133,102 @@ public class Lesson {
         lesson.isNoLesson = true;
 
         return lesson;
+    }
+
+    public static class Builder {
+        private String disciplineName;
+        private long beginMillis;
+        private long endMillis;
+        private String classroom;
+        private String teacherName;
+        private int orderInDay;
+        private boolean isLecture;
+        private boolean isSeminar;
+        private boolean isLaboratoryWork;
+        private boolean isPhysicalEducation;
+        private boolean isMilitaryLesson;
+
+        public Builder discipline(String disciplineName) {
+            this.disciplineName = Preconditions.checkNotNull(disciplineName, "disciplineName == null");
+            return this;
+        }
+
+        public Builder beginAt(long millis) {
+            if (millis < 0) {
+                throw new IllegalArgumentException("millis < 0. Millis = " + millis);
+            }
+            this.beginMillis = millis;
+            return this;
+        }
+
+        public Builder endAt(long millis) {
+            if (millis < 0) {
+                throw new IllegalArgumentException("millis < 0. Millis = " + millis);
+            }
+            this.endMillis = millis;
+            return this;
+        }
+
+        public Builder classroom(String classroom) {
+            this.classroom = Preconditions.checkNotNull(classroom, "classroom == null");
+            return this;
+        }
+
+        public Builder teacher(String teacher) {
+            this.teacherName = Preconditions.checkNotNull(teacher, "teacher == null");
+            return this;
+        }
+
+        public Builder orderInDay(int orderInDay) {
+            if (orderInDay < 0) {
+                throw new IllegalArgumentException("orderInDay < 0, orderInDay = " + orderInDay);
+            }
+            this.orderInDay = orderInDay;
+            return this;
+        }
+
+        public Builder lecture(boolean isLecture) {
+            this.isLecture = isLecture;
+            return this;
+        }
+
+        public Builder seminar(boolean isSeminar) {
+            this.isSeminar = isSeminar;
+            return this;
+        }
+
+        public Builder laboratoryWork(boolean isLaboratoryWork) {
+            this.isLaboratoryWork = isLaboratoryWork;
+            return this;
+        }
+
+        public Builder physicalEducation(boolean isPhysicalEducation) {
+            this.isPhysicalEducation = isPhysicalEducation;
+            return this;
+        }
+
+        public Builder militaryLesson(boolean isMilitaryLesson) {
+            this.isMilitaryLesson = isMilitaryLesson;
+            return this;
+        }
+
+        public Lesson build(){
+            ensureRequiredFieldsSet();
+            return new Lesson(this);
+        }
+
+        private void ensureRequiredFieldsSet(){
+            if (disciplineName == null) {
+                throw new IllegalStateException("disciplineName must be set");
+            }
+
+            if (classroom == null) {
+                throw new IllegalStateException("classroom must be set");
+            }
+
+            if (teacherName == null) {
+                throw new IllegalStateException("teacherName must be set");
+            }
+        }
     }
 }
